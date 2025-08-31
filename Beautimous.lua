6cb78b7b59daf6488790cb7111e05e53b1ddcb51
mikekthx <173126+mikekthx@users.noreply.github.com>
2025-08-31 04:51:03 -0500

    Added missing CVars, alphabetized lists, & added clamping logic

--- Beautimous.lua
+++ Beautimous.lua
@@ -4,49 +4,55 @@ local EXACT = {
 	characterambient = "1",
 	componentemissive = "1",
 	componentspecular = "1",
-	specular = "1",
 	detaildoodadalpha = "100",
 	doodadloddist = "2000",
 	entityloddist = "100",
 	environmentdetail = "150",
-	weatherdensity = "3",
-	worldbasemip = "0",
-	terrainmiplevel = "0",
 	extshadowquality = "5",
-	shadowtexturesize = "2048",
-	overridefarclip = "1",
+	ffxdeath = "1",
+	ffxglow = "1",
+	ffxnetherworld = "1",
+	ffxspecial = "1",
+	gxtexturecachesize = "512",
 	horizonfarclip = "6226",
 	horizonfarclipscale = "6",
 	lodobjectcullsize = "1",
 	lodobjectfadescale = "300",
 	lodobjectminsize = "1",
-	smallcull = "0",
-	ffxglow = "1",
-	ffxnetherworld = "1",
-	ffxspecial = "1",
-	ffxdeath = "1",
-	waterdetail = "3",
+	m2faster = "3",
+	objectfade = "0",
+	overridefarclip = "1",
+	particlemtdensity = "100",
+	projectedtextures = "1",
 	reflectionmode = "3",
 	rippledetail = "3",
-	particlemtdensity = "100",
+	screenshotquality = "10",
+	shadowtexturesize = "2048",
+	showfootprintparticles = "1",
+	showfootprints = "1",
+	skycloudlod = "3",
+	smallcull = "0",
 	spelleffectlevel = "150",
-	sunshafts = "2",
+	specular = "1",
 	ssao = "2",
 	ssaoblur = "2",
 	ssaodistance = "750",
+	sunshafts = "2",
+	terrainmiplevel = "0",
 	texturefilteringmode = "5",
-	showfootprintparticles = "1",
-	showfootprints = "1",
 	violencelevel = "5",
-	projectedtextures = "1",
+	waterdetail = "3",
+	weatherdensity = "3",
+	worldbasemip = "0",
 }
 
 local FLEX = {
 	farclip = {"1600","1250","1000","800","600","500","400","300","200"},
+	groundeffectdensity = {"256","128","64","32","16","8","4","2","1"},
 	groundeffectdist = {"600","400","300","256","200","180","160","150","140"},
+	gxmultisample = {"8","4","2","1"},
 	particledensity = {"100","1.0","1","0.9","0.8","0.75","0.5"},
 	skycloudlod = {"3","2","1","0"},
-	gxmultisample = {"8","4","2","1"},
 }
 
 local ALIAS = {
@@ -56,34 +62,53 @@ local ALIAS = {
 	detaildoodadalpha = {"detailDoodadAlpha","DetailDoodadAlpha"},
 	doodadloddist = {"doodadLodDist","doodadLODDist","DoodadLODDist"},
 	entityloddist = {"entityLodDist","entityLODDist","EntityLODDist"},
+	environmentdetail = {"environmentDetail","EnvironmentDetail"},
 	extshadowquality = {"extShadowQuality","ExtShadowQuality"},
+	ffxdeath = {"ffxDeath","FfxDeath","FFXDeath"},
 	ffxglow = {"ffxGlow","FfxGlow","FFXGlow"},
 	ffxnetherworld = {"ffxNetherworld","FfxNetherworld","FFXNetherworld"},
 	ffxspecial = {"ffxSpecial","FfxSpecial","FFXSpecial"},
-	ffxdeath = {"ffxDeath","FfxDeath","FFXDeath"},
+	farclip = {"farClip","Farclip","FarClip"},
 	groundeffectdist = {"groundEffectDist","GroundEffectDist"},
 	groundeffectfade = {"groundEffectFade","GroundEffectFade"},
 	groundeffectdensity = {"groundEffectDensity","GroundEffectDensity"},
+	gxmultisample = {"gxMultisample","GxMultisample","GxMultiSample","GXMultisample","GXMultiSample"},
+	gxtexturecachesize = {"gxTextureCacheSize","GxTextureCacheSize","GXTextureCacheSize"},
 	horizonfarclip = {"horizonFarclip","horizonFarClip","HorizonFarClip"},
 	horizonfarclipscale = {"horizonFarclipScale","horizonFarClipScale","HorizonFarClipScale"},
 	lodobjectculldist = {"lodObjectCullDist","LODObjectCullDist"},
 	lodobjectcullsize = {"lodObjectCullSize","LODObjectCullSize"},
 	lodobjectfadescale = {"lodObjectFadeScale","LODObjectFadeScale"},
 	lodobjectminsize = {"lodObjectMinSize","LODObjectMinSize"},
+	m2faster = {"m2Faster","M2Faster"},
+	objectfade = {"objectFade","ObjectFade"},
 	overridefarclip = {"overrideFarclip","overrideFarClip","OverrideFarClip"},
 	particledensity = {"particleDensity","ParticleDensity"},
 	particlemtdensity = {"particleMTDensity","ParticleMTDensity"},
 	projectedtextures = {"projectedTextures","ProjectedTextures"},
+	reflectionmode = {"reflectionMode","ReflectionMode"},
+	rippledetail = {"rippleDetail","RippleDetail"},
+	screenshotquality = {"screenshotQuality","ScreenshotQuality","ScreenShotQuality"},
 	shadowtexturesize = {"shadowTextureSize","ShadowTextureSize"},
+	showfootprintparticles = {"showFootprintParticles","ShowFootprintParticles"},
+	showfootprints = {"showFootprints","ShowFootprints"},
 	skycloudlod = {"skyCloudLod","skyCloudLOD","SkyCloudLOD"},
+	smallcull = {"smallCull","SmallCull"},
+	specular = {"Specular"},
 	spelleffectlevel = {"spellEffectLevel","SpellEffectLevel"},
+	ssao = {"Ssao","SSAO"},
 	ssaoblur = {"ssaoBlur","SsaoBlur","SSAOBlur"},
-	texturefilteringmode = {"textureFilteringMode","TextureFilteringMode"},
+	ssaodistance = {"ssaoDistance","SsaoDistance","SSAODistance"},
+	sunshafts = {"sunShafts","SunShafts"},
 	terrainmiplevel = {"terrainMipLevel","TerrainMipLevel"},
+	texturefilteringmode = {"textureFilteringMode","TextureFilteringMode"},
+	violencelevel = {"violenceLevel","ViolenceLevel"},
+	waterdetail = {"waterDetail","WaterDetail"},
+	weatherdensity = {"weatherDensity","WeatherDensity"},
 	worldbasemip = {"worldBaseMip","WorldBaseMip"},
 }
 
-local lastChanged, lastSkipped = {}, {}
+local lastChanged, lastSkipped, lastClamped = {}, {}, {}
 
 local function variantsFor(name)
 	local t = {name}
@@ -106,7 +131,7 @@ local function firstExistingCVar(...)
 	end
 end
 
-local function setExact(name, value, changed, skipped)
+local function setExact(name, value, changed, skipped, clamped)
 	local key = firstExistingCVar(unpack(variantsFor(name)))
 	if not key then
 		table.insert(skipped, name)
@@ -115,13 +140,16 @@ local function setExact(name, value, changed, skipped)
 	local cur = GetCVar(key)
 	if cur ~= value then
 		SetCVar(key, value)
-		if GetCVar(key) == value then
+		local after = GetCVar(key)
+		if after == value then
 			table.insert(changed, key .. "=" .. value)
+		elseif after ~= cur then
+			table.insert(clamped, key .. " (" .. value .. "→" .. after .. ")")
 		end
 	end
 end
 
-local function setMax(name, list, changed, skipped)
+local function setMax(name, list, changed, skipped, clamped)
 	local key = firstExistingCVar(unpack(variantsFor(name)))
 	if not key then
 		table.insert(skipped, name)
@@ -136,45 +164,55 @@ local function setMax(name, list, changed, skipped)
 				table.insert(changed, key .. "=" .. after)
 			end
 			return
+		elseif after ~= original then
+			table.insert(clamped, key .. " (" .. v .. "→" .. after .. ")")
+			return
 		end
 	end
 end
 
 local function apply()
-	local changed, skipped = {}, {}
+	local changed, skipped, clamped = {}, {}, {}
 
 	for k, v in pairs(EXACT) do
-		setExact(k, v, changed, skipped)
+		setExact(k, v, changed, skipped, clamped)
 	end
 
-	setMax("farclip", FLEX.farclip, changed, skipped)
-	setMax("groundeffectdist", FLEX.groundeffectdist, changed, skipped)
-	setMax("particledensity", FLEX.particledensity, changed, skipped)
-	setMax("skycloudlod", FLEX.skycloudlod, changed, skipped)
-	setMax("gxmultisample", FLEX.gxmultisample, changed, skipped)
+	setMax("farclip", FLEX.farclip, changed, skipped, clamped)
+	setMax("groundeffectdist", FLEX.groundeffectdist, changed, skipped, clamped)
+	setMax("groundeffectdensity", FLEX.groundeffectdensity, changed, skipped, clamped)
+	setMax("particledensity", FLEX.particledensity, changed, skipped, clamped)
+	setMax("skycloudlod", FLEX.skycloudlod, changed, skipped, clamped)
+	setMax("gxmultisample", FLEX.gxmultisample, changed, skipped, clamped)
 
-	do
-		local fadeKey = firstExistingCVar(unpack(variantsFor("groundeffectfade")))
-		if fadeKey then
-			local distKey = firstExistingCVar(unpack(variantsFor("groundeffectdist"))) or "groundeffectdist"
-			local want = GetCVar(distKey)
-			if want and GetCVar(fadeKey) ~= want then
-				SetCVar(fadeKey, want)
-				table.insert(changed, fadeKey .. "=" .. GetCVar(fadeKey))
+	local fadeKey = firstExistingCVar(unpack(variantsFor("groundeffectfade")))
+	if fadeKey then
+		local distKey = firstExistingCVar(unpack(variantsFor("groundeffectdist"))) or "groundeffectdist"
+		local want = GetCVar(distKey)
+		if want and GetCVar(fadeKey) ~= want then
+			SetCVar(fadeKey, want)
+			local after = GetCVar(fadeKey)
+			if after == want then
+				table.insert(changed, fadeKey .. "=" .. after)
+			else
+				table.insert(clamped, fadeKey .. " (" .. want .. "→" .. after .. ")")
 			end
-		else
-			table.insert(skipped, "groundeffectfade")
 		end
+	else
+		table.insert(skipped, "groundeffectfade")
 	end
 
-	lastChanged, lastSkipped = changed, skipped
+	lastChanged, lastSkipped, lastClamped = changed, skipped, clamped
 end
 
 local function printReport()
 	if #lastChanged > 0 then
 		DEFAULT_CHAT_FRAME:AddMessage("Beautimous set: " .. table.concat(lastChanged, ", "), 0.3, 1, 0.3)
 	else
-		DEFAULT_CHAT_FRAME:AddMessage("Beautimous made no changes.", 0.7, 0.7, 0.7)
+		DEFAULT_CHAT_FRAME:AddMessage("Beautimous made no changes", 0.7, 0.7, 0.7)
+	end
+	if #lastClamped > 0 then
+		DEFAULT_CHAT_FRAME:AddMessage("Beautimous clamped: " .. table.concat(lastClamped, ", "), 1, 0.8, 0.2)
 	end
 	if #lastSkipped > 0 then
 		DEFAULT_CHAT_FRAME:AddMessage("Beautimous skipped: " .. table.concat(lastSkipped, ", "), 1, 0.4, 0.4)
